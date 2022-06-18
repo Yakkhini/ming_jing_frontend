a1b39cd8c8315a09d7bdfd39f8a81b37d24bbff9
Yakkhini <59007159+Yakkhini@users.noreply.github.com>
2022-06-19 01:27:01 +0800

    Feat: Big Title component.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.svg" />
+  <BigTitle msg="明镜" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BigTitle from "./components/BigTitle.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    BigTitle,
+  },
+};
 </script>
 
 <style>
